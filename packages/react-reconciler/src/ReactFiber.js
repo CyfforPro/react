e595fd4afa7da86e177efcc510730d913800fabe
CyfforPro <ycyf253256@163.com>
2020-06-15 21:01:35 +0800

    FiberRoot和Fiber对象数据结构解读，fiber tree单向链表的形成见issue #1

--- packages/react-reconciler/src/ReactFiber.js
+++ packages/react-reconciler/src/ReactFiber.js
@@ -95,19 +95,24 @@ export type Fiber = {|
   // minimize the number of objects created during the initial render.
 
   // Tag identifying the type of fiber.
+  // 标记fiber（组件）类型
   tag: WorkTag,
 
   // Unique identifier of this child.
+  // ReactElement里面的key
   key: null | string,
 
   // The value of element.type which is used to preserve the identity during
   // reconciliation of this child.
+  // ReactElement.type，也就是我们调用createElement的第一个参数
   elementType: any,
 
   // The resolved function/class/ associated with this fiber.
+  // 异步组件（lazy load）resolved之后返回的内容，一般是`function`或者`class`
   type: any,
 
   // The local state associated with this fiber.
+  // 跟当前Fiber相关的本地状态（比如浏览器环境就是DOM节点）
   stateNode: any,
 
   // Conceptual aliases
@@ -120,28 +125,39 @@ export type Fiber = {|
   // This is effectively the parent, but there can be multiple parents (two)
   // so this is only the parent of the thing we're currently processing.
   // It is conceptually the same as the return address of a stack frame.
+  // 指向他在Fiber节点树中的parent，用来在处理完这个节点之后向上返回
   return: Fiber | null,
 
   // Singly Linked List Tree Structure.
+  // 单链表树结构
+  // 指向自己的第一个子节点
   child: Fiber | null,
+  // 指向自己的兄弟结构
+  // 兄弟节点的return指向同一个父节点
   sibling: Fiber | null,
   index: number,
 
   // The ref last used to attach this node.
   // I'll avoid adding an owner field for prod and model that as functions.
+  // ref属性
   ref: null | (((handle: mixed) => void) & {_stringRef: ?string}) | RefObject,
 
   // Input is the data coming into process this fiber. Arguments. Props.
+  // 新的更新带来的新的props
   pendingProps: any, // This type will be more specific once we overload the tag.
+  // 上一次渲染完成之后的props
   memoizedProps: any, // The props used to create the output.
 
   // A queue of state updates and callbacks.
+  // 该Fiber对应的组件产生的Update会存放在这个队列里面
   updateQueue: UpdateQueue<any> | null,
 
   // The state used to create the output
+  // 上一次渲染的时候的state
   memoizedState: any,
 
   // A linked-list of contexts that this fiber depends on
+  // 一个列表，存放这个Fiber依赖的context
   contextDependencies: ContextDependencyList | null,
 
   // Bitfield that describes properties about the fiber and its subtree. E.g.
@@ -150,36 +166,53 @@ export type Fiber = {|
   // parent. Additional flags can be set at creation time, but after that the
   // value should remain unchanged throughout the fiber's lifetime, particularly
   // before its child fibers are created.
+
+  // 用来描述当前Fiber和他的子树的Bitfield
+  // ConcurrentMode flag表示这个子树是否默认是异步渲染的
+  // Fiber被创建的时候他会继承父Fiber的mode
+  // 其他的标识也可以在创建的时候被设置
+  // 但是在创建之后不应该再被修改，特别是在他的子Fiber创建之前
   mode: TypeOfMode,
 
   // Effect
+  // 用来记录Side Effect
   effectTag: SideEffectTag,
 
   // Singly linked list fast path to the next fiber with side-effects.
+  // 单链表用来快速查找下一个side effect
   nextEffect: Fiber | null,
 
   // The first and last fiber with side-effect within this subtree. This allows
   // us to reuse a slice of the linked list when we reuse the work done within
   // this fiber.
+  // 子树中第一个side effect
   firstEffect: Fiber | null,
+  // 子树中最后一个side effect
   lastEffect: Fiber | null,
 
   // Represents a time in the future by which this work should be completed.
   // Does not include work found in its subtree.
+  // 代表任务在未来的哪个时间点应该被完成
+  // 不包括他的子树产生的任务
   expirationTime: ExpirationTime,
 
   // This is used to quickly determine if a subtree has no pending changes.
+  // 快速确定子树中是否有不在等待的变更
   childExpirationTime: ExpirationTime,
 
   // This is a pooled version of a Fiber. Every fiber that gets updated will
   // eventually have a pair. There are cases when we can clean up pairs to save
   // memory if we need to.
+  // 在Fiber树更新的过程中，每个Fiber都会有一个跟其对应的Fiber
+  // 我们称他为`current <==> workInProgress`
+  // 在渲染完成之后他们会交换位置
   alternate: Fiber | null,
 
   // Time spent rendering this Fiber and its descendants for the current update.
   // This tells us how well the tree makes use of sCU for memoization.
   // It is reset to 0 each time we render and only updated when we don't bailout.
   // This field is only set when the enableProfilerTimer flag is enabled.
+  // 下面是调试相关的，收集每个Fiber和子树渲染时间的
   actualDuration?: number,
 
   // If the Fiber is currently active in the "render" phase,
@@ -309,6 +342,11 @@ function FiberNode(
 //    is faster.
 // 5) It should be easy to port this to a C struct and keep a C implementation
 //    compatible.
+
+// Fiber——
+// 1. 每一个ReactElement对应一个Fiber对象
+// 2. 记录节点的各种状态——使得在fc可以有state等"存储"的信息
+// 3. 串联整个应用形成树结构
 const createFiber = function(
   tag: WorkTag,
   pendingProps: mixed,
--- packages/react-reconciler/src/ReactFiberRoot.js
+++ packages/react-reconciler/src/ReactFiberRoot.js
@@ -31,10 +31,13 @@ export type PendingInteractionMap = Map<ExpirationTime, Set<Interaction>>;
 
 type BaseFiberRootProperties = {|
   // Any additional information from the host associated with this root.
+  // root节点，render方法中接收的第二个参数
   containerInfo: any,
   // Used only by persistent updates.
+  // 持久化更新时用到，也就是不支持增量更新的平台，在正常的client渲染中用不到，ssr中用
   pendingChildren: any,
   // The currently active root fiber. This is the mutable root of the tree.
+  // 当前应用对应的Fiber对象，是Root Fiber
   current: Fiber,
 
   // The following priority levels are used to distinguish between 1)
@@ -42,14 +45,22 @@ type BaseFiberRootProperties = {|
   // work that may be unsuspended. We choose not to track each individual
   // pending level, trading granularity for performance.
   //
+  // 以下优先级是用来区分不同的work的：
+  // 1. 未提交的work
+  // 2. 未提交的且被暂停的work
+  // 3. 未提交的可能会被暂停的work
+  //
   // The earliest and latest priority levels that are suspended from committing.
+  // 最早及最新的被暂停的任务
   earliestSuspendedTime: ExpirationTime,
   latestSuspendedTime: ExpirationTime,
   // The earliest and latest priority levels that are not known to be suspended.
+  // 最早及最新的可能会被暂停的任务（所有任务一开始都是这样的状态）
   earliestPendingTime: ExpirationTime,
   latestPendingTime: ExpirationTime,
   // The latest priority level that was pinged by a resolved promise and can
   // be retried.
+  // 最新的通过一个promise被reslove并且可以重新尝试的任务
   latestPingedTime: ExpirationTime,
 
   pingCache:
@@ -60,28 +71,40 @@ type BaseFiberRootProperties = {|
   // If an error is thrown, and there are no more updates in the queue, we try
   // rendering from the root one more time, synchronously, before handling
   // the error.
+  // 如果有错误被抛出并且没有更多的更新存在，我们尝试在处理错误前同步重新从头渲染
+  // 在renderRoot出现无法处理的错误时会被设置为true
   didError: boolean,
 
+  // 正在等待提交的任务的expirationTime
   pendingCommitExpirationTime: ExpirationTime,
   // A finished work-in-progress HostRoot that's ready to be committed.
+  // 已经完成的任务的FiberRoot对象，如果你只有一个Root，那他永远只可能是这个Root对应的Fiber，或者是null
+  // 在commit阶段只会处理这个值对应的任务
   finishedWork: Fiber | null,
   // Timeout handle returned by setTimeout. Used to cancel a pending timeout, if
   // it's superseded by a new one.
+  // 在任务被挂起的时候通过setTimeout设置返回内容，用来下一次如果有新的任务挂起时清理还没触发的timeout
   timeoutHandle: TimeoutHandle | NoTimeout,
   // Top context object, used by renderSubtreeIntoContainer
+  // 顶层context对象，只有主动调用`renderSubtreeIntoContainer`时才会有用，一般用不到
   context: Object | null,
   pendingContext: Object | null,
   // Determines if we should attempt to hydrate on the initial mount
+  // 用来确定第一次渲染的时候是否需要hydrate
   +hydrate: boolean,
   // Remaining expiration time on this root.
   // TODO: Lift this into the renderer
+  // 当前root上剩余的过期时间
   nextExpirationTimeToWorkOn: ExpirationTime,
+  // 当前更新对应的过期时间
   expirationTime: ExpirationTime,
   // List of top-level batches. This list indicates whether a commit should be
   // deferred. Also contains completion callbacks.
   // TODO: Lift this into the renderer
+  // 没啥用，先不管
   firstBatch: Batch | null,
   // Linked-list of roots
+  // root之间关联的链表结构，存在一个以上FiberRoot时才会用到
   nextScheduledRoot: FiberRoot | null,
 |};
 
@@ -105,6 +128,10 @@ export type FiberRoot = {
   ...ProfilingOnlyFiberRootProperties,
 };
 
+// FiberRoot——
+// 1. 整个应用的起点
+// 2. 包含应用挂载的目标节点
+// 3. 记录整个应用更新过程中的各种信息
 export function createFiberRoot(
   containerInfo: any,
   isConcurrent: boolean,
